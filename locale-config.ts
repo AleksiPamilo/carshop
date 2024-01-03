6ddec8b50a57a83612af3b0f8984ab6c0d5b259e
AleksiPamilo <aleksi.pamilo@live.com>
2024-01-03 02:55:00 +0200

    Initial Commit

--- locale-config.ts
+++
@@ -1,6 +0,0 @@
-export const localeConfig = {
-    defaultLocale: "fi",
-    locales: ["fi", "en"],
-} as const;
-
-export type Locale = typeof localeConfig.locales[number];
--- src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-import { useDictionary } from "@/components/context/DictionaryProvider";
-
-export {
-    useDictionary,
-};
