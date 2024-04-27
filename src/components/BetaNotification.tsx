79d6c2ea617a2c0d76964d1e5f8fbd8636fb5d5f
AleksiPamilo <aleksi.pamilo@live.com>
2024-04-27 13:37:29 +0300

    modify beta-notification

--- src/components/BetaNotification.tsx
+++ src/components/BetaNotification.tsx
@@ -17,9 +17,9 @@ export default function BetaNotification({ dictionary }: { dictionary: IDictiona
     }, []);
 
     return (
-        <dialog className="bg-transparent w-screen h-screen z-50 fixed justify-center items-center" style={{ display: open ? "flex" : "none" }} open={open}>
-            <div className="flex flex-col gap-24 items-center justify-center w-full mt-12">
-                <div className="max-w-[40rem] min-h-[15rem] mb-12 shadow-xl rounded-md mx-3 p-4 flex flex-col gap-4 items-center justify-center border bg-zinc-100 dark:bg-zinc-900">
+        <dialog className="bg-transparent backdrop-blur-sm w-full h-screen z-[150] fixed justify-center items-center px-3" style={{ display: open ? "flex" : "none" }} open={open}>
+            <div className="flex flex-col items-center justify-center w-full mt-12">
+                <div className="max-w-[40rem] min-h-[15rem] mb-12 shadow-xl rounded-md p-4 flex flex-col gap-4 items-center justify-center border bg-zinc-100 dark:bg-zinc-900">
                     <h1 className="font-semibold text-xl">{dictionary.beta.title}</h1>
                     <p>
                         {dictionary.beta.description}
